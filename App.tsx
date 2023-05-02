35233b64fe0adb18b51f9b44e8f5733c46a16ae4
tshrusd <tshr.usd@gmail.com>
2023-05-02 23:13:29 +0900

    eslint

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import TestScreen from './src/screens/TestScreen';
+
+const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TestScreen message='ReactNative'/>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/screens/TestScreen.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+interface ITestScreenProps {
+  message?: string;
+}
+
+const TestScreen: React.FC<ITestScreenProps> = ({message = 'World'}: ITestScreenProps) => {
+  // const {message} = props;
+  return (
+    <View style={styles.testScreen}>
+      <Text>{`Hello ${message}`}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  testScreen: {
+    flex: 1,
+    backgroundColor: 'gray',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+export default TestScreen;
