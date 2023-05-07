3ef7615bde7b8206166d85a1d1e8683de28631a0
tshrusd <tshr.usd@gmail.com>
2023-05-07 09:27:40 +0900

    Form

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -1,30 +1,64 @@
 import React, { useCallback } from 'react';
 import { View, StyleSheet, GestureResponderEvent } from 'react-native';
-import { Input, Button } from '@rneui/themed';
+import { Button } from '@rneui/themed';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 import { useScreenNavigation } from '../screens/navigation';
+import { FormTextInput, Form, schema } from './FormTextInput';
 
 type UserFormProps = {
   onSubmit?: (event?: GestureResponderEvent) => void;
 };
 
 const UserForm: React.FC<UserFormProps> = (props) => {
-  const { onSubmit } = props;
   const navigation = useScreenNavigation();
-  const handleSubmit = useCallback(() => {
-    if (onSubmit) {
-      onSubmit();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isDirty },
+  } = useForm<Form>({
+    mode: 'all',
+    defaultValues: { email: '', password: '' },
+    resolver: yupResolver(schema),
+  });
+  const isInvalid: boolean = Object.keys(errors).length > 0 || !isDirty;
+
+  const onValid = (data: Form) => {
+    console.log(data);
+    if (props.onSubmit) {
+      props.onSubmit();
     }
     navigation.reset({
       index: 0,
       routes: [{ name: 'MemoList' }],
     });
-  }, [navigation, onSubmit]);
+  };
+
   return (
     <View style={styles.form}>
-      <Input placeholder="Email Adress" />
-      <Input placeholder="Passward" />
-      <Button title="Submit" onPress={handleSubmit} />
+      <FormTextInput
+        control={control}
+        areaName="email"
+        label="メールアドレス"
+        placeholder="Email Adress"
+        autoComplete="email"
+        autoCapitalize="none"
+      />
+      <FormTextInput
+        control={control}
+        areaName="password"
+        label="パスワード"
+        placeholder="Password"
+        autoComplete="password"
+        autoCapitalize="none"
+        secureTextEntry
+      />
+      <Button
+        title="Submit"
+        onPress={(event: GestureResponderEvent) => void handleSubmit(onValid)(event)}
+        disabled={isInvalid}
+      />
     </View>
   );
 };
